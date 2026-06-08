8ee118a4909191e122255f397ce29f97326b25b4
한솔 <hsol73076@gmail.com>
2026-06-08 16:26:07 +0900

    chore : ktlint 플러그인 제거 및 인텔리제이 내장 포맷터 연동

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     id("org.springframework.boot") version "4.0.6"
     id("io.spring.dependency-management") version "1.1.7"
     kotlin("plugin.jpa") version "2.2.21"
-    id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
 }
 
 group = "team.joup"
@@ -52,13 +51,6 @@ allOpen {
     annotation("jakarta.persistence.Entity")
     annotation("jakarta.persistence.MappedSuperclass")
     annotation("jakarta.persistence.Embeddable")
-}
-
-ktlint {
-    version.set("1.3.1")
-    verbose.set(true)
-    outputToConsole.set(true)
-    coloredOutput.set(true)
 }
 
 tasks.withType<Test> {
