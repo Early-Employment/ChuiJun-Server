3820d3202ef06bbb8f6cf692a91659e87c50a70a
한솔 <hsol73076@gmail.com>
2026-06-16 16:19:19 +0900

    refactor: 문제 리스트 페이징에 최신순(id, DESC) 기본 정렬 조건 추가

--- src/main/kotlin/team/joup/chuijun/domain/problem/controller/ProblemController.kt
+++ src/main/kotlin/team/joup/chuijun/domain/problem/controller/ProblemController.kt
@@ -9,6 +9,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
 import org.springframework.data.web.PageableDefault
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -33,7 +34,7 @@ class ProblemController(
     ])
     @GetMapping
     fun getProblems(
-        @PageableDefault(size = 20) pageable: Pageable
+        @PageableDefault(size = 20, sort = ["id"], direction = Sort.Direction.DESC) pageable: Pageable
     ): ResponseEntity<Page<GetProblemListResponse>> {
         val response = problemService.getProblemList(pageable)
         return ResponseEntity.ok(response)
