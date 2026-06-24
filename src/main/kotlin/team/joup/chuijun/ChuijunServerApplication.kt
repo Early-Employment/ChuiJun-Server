f0023c772a6c62f531ccd8dfdcb363b64990ccf9
한솔 <hsol73076@gmail.com>
2026-06-24 21:09:08 +0900

    EnableConfigurationProperties 어노테이션 추가

--- src/main/kotlin/team/joup/chuijun/ChuijunServerApplication.kt
+++ src/main/kotlin/team/joup/chuijun/ChuijunServerApplication.kt
@@ -2,10 +2,13 @@ package team.joup.chuijun
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
+import team.joup.chuijun.domain.auth.config.DataGsmOAuthProperties
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
+@EnableConfigurationProperties(DataGsmOAuthProperties::class)
 class ChuijunServerApplication
 
 fun main(args: Array<String>) {
